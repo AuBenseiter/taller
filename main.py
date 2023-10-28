4c10f9ba2b23f734bd0e860868bca4efb3ef6446
AuBenseiter <daniel.frias.matias@gmail.com>
2023-10-28 03:14:02 -0300

    Change: it change the UI Manage

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from Manager import InicioView
+import sys
+from PyQt5.QtWidgets import QApplication
+from Manager import ManagerView
 
 if __name__ == "__main__":
-    app = InicioView()
-    app.mainloop()
+    app = QApplication(sys.argv)
+    window = ManagerView()
+    window.setupUi(window)  # Llama a setupUi para configurar la interfaz
+    window.show()
+    sys.exit(app.exec_())
